a9714e856374578c94c3bbc6c36be272f3443bb8
Amateo642 <KitlyaevDD@mail.ru>
2020-09-23 18:06:37 +0300

    Final deploy

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import { Button, Container, Form, FormControl, Nav, Navbar } from "react-bootstrap";
 import React, { Component } from 'react'
 import logo from './logo.png'
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import Home from '../Pages/Home';
 import About from '../Pages/About';
@@ -11,7 +11,7 @@ import Blog from '../Pages/Blog';
 export default class Header extends Component {
     render() {
         return (
-            <>
+            <Router basename="/">
                 <Navbar /*fixed="top"*/ collapseOnSelect expand="md" bg="dark" variant="dark" >
                     <Container>
                         <Navbar.Brand href="/">
@@ -26,10 +26,10 @@ export default class Header extends Component {
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav" >
                             <Nav className="mr-auto">
-                                <Nav.Link href="/"> Home </Nav.Link>
-                                <Nav.Link href="/about"> About us </Nav.Link>
-                                <Nav.Link href="/contacts"> Contacts </Nav.Link>
-                                <Nav.Link href="/blog"> Blog </Nav.Link>
+                                <Nav.Link as={Link} to="/"> Home </Nav.Link>
+                                <Nav.Link as={Link} to="/about"> About us </Nav.Link>
+                                <Nav.Link as={Link} to="/contacts"> Contacts </Nav.Link>
+                                <Nav.Link as={Link} to="/blog"> Blog </Nav.Link>
                             </Nav>
                             <Form inline >
                                 <FormControl
@@ -42,16 +42,13 @@ export default class Header extends Component {
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
-
-                <Router>
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/about" component={About} />
-                        <Route exact path="/contacts" component={Contacts} />
-                        <Route exact path="/blog" component={Blog} />
-                    </Switch>
-                </Router>
-            </>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/contacts" component={Contacts} />
+                    <Route exact path="/blog" component={Blog} />
+                </Switch>                  
+            </Router>
         )
     }
 }
